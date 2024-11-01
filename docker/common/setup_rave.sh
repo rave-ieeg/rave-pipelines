cbd6181743ce40c4d39deac23604254fe5df42ca
dipterix <dipterix.wang@gmail.com>
2024-10-31 22:27:45 -0400

    Docker, updated

--- docker/common/setup_rave.sh
+++ docker/common/setup_rave.sh
@@ -12,11 +12,11 @@ R -q -e 'ravemanager::configure_python()'
 R -q -e 'rpymat::run_command("conda remove -n r-reticulate --all -y")'
 R -q -e 'ravemanager::finalize_installation()'
 
-rm -rf /data/rave_data/raw_dir/DemoSubject
-rm -rf /data/rave_data/raw_dir/yael_demo_001
-rm -rf /data/rave_data/data_dir/demo
-
 # Clean cache
 R -q -e 'rpymat::run_command("pip cache purge")'
 R -q -e 'rpymat::run_command("conda clean -a -y")'
 
+rm -rf /data/rave_data/raw_dir/DemoSubject
+rm -rf /data/rave_data/raw_dir/yael_demo_001
+rm -rf /data/rave_data/data_dir/demo
+rm -rf /tmp/*
