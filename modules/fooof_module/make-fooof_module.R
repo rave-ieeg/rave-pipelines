877f41017801682c9d5e38d202cc9da183c5f113
dipterix <dipterix.wang@gmail.com>
2025-02-28 16:41:16 -0500

    Update for tutorial

--- modules/fooof_module/make-fooof_module.R
+++ modules/fooof_module/make-fooof_module.R
@@ -17,12 +17,105 @@ rm(._._env_._.)
     quote({
         yaml::read_yaml(settings_path)
     }), deps = "settings_path", cue = targets::tar_cue("always")), 
-    input_col = targets::tar_target_raw("col", quote({
-        settings[["col"]]
-    }), deps = "settings"), input_pch = targets::tar_target_raw("pch", 
+    input_random_generator = targets::tar_target_raw("random_generator", 
         quote({
-            settings[["pch"]]
-        }), deps = "settings"), input_n = targets::tar_target_raw("n", 
+            settings[["random_generator"]]
+        }), deps = "settings"), input_sample_size = targets::tar_target_raw("sample_size", 
         quote({
-            settings[["n"]]
-        }), deps = "settings"))
+            settings[["sample_size"]]
+        }), deps = "settings"), generate_input_data = targets::tar_target_raw(name = "input_data", 
+        command = quote({
+            .py_error_handler <- function(e, use_py_last_error = TRUE) {
+                if (use_py_last_error) {
+                  e2 <- asNamespace("reticulate")$py_last_error()
+                  if (!is.null(e2)) {
+                    e <- e2
+                  }
+                }
+                code <- c("import numpy as np", "", "# sample size as integer", 
+                "sample_s = int(sample_size)", "", "if sample_s <= 0:", 
+                "  raise Exception(f\"Sample size should be positive\")", 
+                "", "# get random number generator", "if random_generator == \"randn\":", 
+                "  generator = np.random.randn", "elif random_generator == \"randint\":", 
+                "  generator = np.random.randint", "elif random_generator == \"rand\":", 
+                "  generator = np.random.rand", "else:", "  raise Exception(f\"Unknown random number generator { generator_name }\")", 
+                "", "input_data = {", "  'x' : generator(sample_s),", 
+                "  'y' : generator(sample_s)", "}")
+                stop(sprintf("Target [%s] (python) encountered the following error: \n%s\nAnalysis pipeline code:\n# ---- Target python code: %s -----\n%s\n# ---------------------------------------", 
+                  "input_data", paste(e$message, collapse = "\n"), 
+                  "input_data", paste(code, collapse = "\n")))
+            }
+            re <- tryCatch(expr = {
+                .env <- environment()
+                if (length(c("sample_size", "random_generator"
+                ))) {
+                  args <- structure(names = c("sample_size", 
+                  "random_generator"), lapply(c("sample_size", 
+                  "random_generator"), get, envir = .env))
+                } else {
+                  args <- list()
+                }
+                module <- asNamespace("raveio")$pipeline_py_module(convert = FALSE, 
+                  must_work = TRUE)
+                target_function <- module$rave_pipeline_adapters["input_data"]
+                re <- do.call(target_function, args)
+                cls <- class(re)
+                if (length(cls) && any(endsWith(cls, "rave_pipeline_adapters.RAVERuntimeException"))) {
+                  error_message <- rpymat::py_to_r(re$`__str__`())
+                  .py_error_handler(simpleError(error_message), 
+                    use_py_last_error = FALSE)
+                }
+                return(re)
+            }, python.builtin.BaseException = .py_error_handler, 
+                python.builtin.Exception = .py_error_handler, 
+                py_error = .py_error_handler, error = function(e) {
+                  traceback(e)
+                  stop(e$message, call. = FALSE)
+                })
+            return(re)
+        }), deps = c("sample_size", "random_generator"), cue = targets::tar_cue("thorough"), 
+        pattern = NULL, iteration = "list", format = asNamespace("raveio")$target_format_dynamic("user-defined-python", 
+            target_export = "input_data")), calculate_correlation = targets::tar_target_raw(name = "correlation", 
+        command = quote({
+            .py_error_handler <- function(e, use_py_last_error = TRUE) {
+                if (use_py_last_error) {
+                  e2 <- asNamespace("reticulate")$py_last_error()
+                  if (!is.null(e2)) {
+                    e <- e2
+                  }
+                }
+                code <- c("import numpy as np", "correlation = np.corrcoef(input_data['x'], input_data['y'])", 
+                "print(correlation)")
+                stop(sprintf("Target [%s] (python) encountered the following error: \n%s\nAnalysis pipeline code:\n# ---- Target python code: %s -----\n%s\n# ---------------------------------------", 
+                  "correlation", paste(e$message, collapse = "\n"), 
+                  "correlation", paste(code, collapse = "\n")))
+            }
+            re <- tryCatch(expr = {
+                .env <- environment()
+                if (length("input_data")) {
+                  args <- structure(names = "input_data", lapply("input_data", 
+                    get, envir = .env))
+                } else {
+                  args <- list()
+                }
+                module <- asNamespace("raveio")$pipeline_py_module(convert = FALSE, 
+                  must_work = TRUE)
+                target_function <- module$rave_pipeline_adapters["correlation"]
+                re <- do.call(target_function, args)
+                cls <- class(re)
+                if (length(cls) && any(endsWith(cls, "rave_pipeline_adapters.RAVERuntimeException"))) {
+                  error_message <- rpymat::py_to_r(re$`__str__`())
+                  .py_error_handler(simpleError(error_message), 
+                    use_py_last_error = FALSE)
+                }
+                return(re)
+            }, python.builtin.BaseException = .py_error_handler, 
+                python.builtin.Exception = .py_error_handler, 
+                py_error = .py_error_handler, error = function(e) {
+                  traceback(e)
+                  stop(e$message, call. = FALSE)
+                })
+            return(re)
+        }), deps = "input_data", cue = targets::tar_cue("thorough"), 
+        pattern = NULL, iteration = "list", format = asNamespace("raveio")$target_format_dynamic("user-defined-python", 
+            target_export = "correlation")))
--- modules/fooof_module/py/fooof_module/rave_pipeline_adapters/pipeline_target_input_data.py
+++ modules/fooof_module/py/fooof_module/rave_pipeline_adapters/pipeline_target_input_data.py
@@ -7,12 +7,29 @@
 from .. import shared
 from . import RAVERuntimeException
 
-def pipeline_target_input_data():
+def pipeline_target_input_data(sample_size, random_generator):
   try:
     import numpy as np
+    
+    # sample size as integer
+    sample_s = int(sample_size)
+    
+    if sample_s <= 0:
+      raise Exception(f"Sample size should be positive")
+    
+    # get random number generator
+    if random_generator == "randn":
+      generator = np.random.randn
+    elif random_generator == "randint":
+      generator = np.random.randint
+    elif random_generator == "rand":
+      generator = np.random.rand
+    else:
+      raise Exception(f"Unknown random number generator { generator_name }")
+    
     input_data = {
-      'x' : np.random.rand(10),
-      'y' : np.random.rand(10)
+      'x' : generator(sample_s),
+      'y' : generator(sample_s)
     }
     return input_data
   except Exception as e:
