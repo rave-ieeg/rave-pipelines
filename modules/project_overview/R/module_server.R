7a4b1773353694961c027c27f7524d6a34dc4481
Guy Silberman <guysilberman1@gmail.com>
2025-07-18 21:05:11 +0300

    Added download button to package and download project overview as a zip file; fixed iframe not updating issue

--- modules/project_overview/R/module_server.R
+++ modules/project_overview/R/module_server.R
@@ -10,21 +10,101 @@ module_server <- function(input, output, session, ...){
   # Local non-reactive values, used to store static variables
   local_data <- dipsaus::fastmap2()
 
+  server_tools <- get_default_handlers(session = session)
+
   # Register event: main pipeline need to run
   shiny::bindEvent(
     ravedash::safe_observe({
       # Zip the
-      src_path <- file.path(pipeline$pipeline_path, "build", "_site")
-      dst_path <- file.path(pipeline$pipeline_path, "build", "reports.zip")
-      if(file.exists(dst_path)) {
-        unlink(dst_path)
-      }
-      utils::zip(zipfile = dst_path, files = src_path)
+      ravedash::show_notification(
+        message = "Packaging the reports in the background...",
+        title = "Exporting",
+        type = "info",
+        autohide = FALSE,
+        session = session,
+        class = ns("export_report")
+      )
+
+      promises::promise(function(resolve, reject) {
+
+        job_id <- ravepipeline::start_job(
+          fun_args = list(pipeline_path = pipeline$pipeline_path),
+          packages = "utils",
+          fun = function(pipeline_path) {
+            src_path <- file.path(pipeline_path, "build", "_site")
+            dst_path <- file.path(pipeline_path, "build", "reports.zip")
+            if(file.exists(dst_path)) {
+              unlink(dst_path)
+            }
+
+            cwd <- getwd()
+            setwd(src_path)
+            on.exit({ setwd(cwd) })
+
+            utils::zip(zipfile = "../reports.zip", files = list.files("."))
+
+            return(dst_path)
+          }
+        )
+
+        Sys.sleep(1)
+
+        tryCatch(
+          {
+            dst_path <- ravepipeline::resolve_job(job_id)
+            ravedash::clear_notifications(class = ns("export_report"))
+
+            shiny::showModal(
+              session = session,
+              shiny::modalDialog(
+                title = "Download packaged reports",
+                easyClose = FALSE,
+                size = "m",
+                footer = shiny::tagList(
+                  shiny::modalButton("Close"),
+                  shiny::downloadButton(
+                    outputId = ns("download_btn"),
+                    label = "Download",
+                    class = "btn-primary",
+                    icon = ravedash::shiny_icons$download
+                  )
+                ),
+                shiny::p(
+                  "The overview reports have been generated at ",
+                  shiny::br(),
+                  dst_path
+                )
+              )
+            )
+
+            resolve(dst_path)
+          },
+          error = function(e) {
+            ravedash::clear_notifications(class = ns("export_report"))
+            ravedash::error_notification(e)
+            resolve(e)
+          }
+        )
+
+      })
+
+      return()
+
     }),
     server_tools$run_analysis_flag(),
     ignoreNULL = TRUE, ignoreInit = TRUE
   )
 
+  output$download_btn <- shiny::downloadHandler(
+    filename = "project_reports.zip",
+    contentType = "application/zip",
+    content = function(con) {
+      dst_path <- file.path(pipeline$pipeline_path, "build", "reports.zip")
+      file.copy(dst_path, con)
+      try(silent = TRUE, { shiny::removeModal(session = session) })
+      return(con)
+    }
+  )
 
   output$report_container <- shiny::renderUI({
 
@@ -34,7 +114,7 @@ module_server <- function(input, output, session, ...){
     )
 
     shiny::tags$iframe(
-      src = sprintf("project_overview/index.html#%s", dipsaus::base64_urlencode(strftime(Sys.time()))),
+      src = sprintf("project_overview/index.html?t=%s", dipsaus::base64_urlencode(strftime(Sys.time()))),
       style = "height:100vh; width:100%; border:none;",
       class = "screen-height"
     )
