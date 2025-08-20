40041696a1b5841bcc9846a040eabf98e9ac6998
dipterix <dipterix.wang@gmail.com>
2025-08-20 12:40:14 -0400

    Allows loader to pull the latest report by name

--- modules/standalone_report/R/loader.R
+++ modules/standalone_report/R/loader.R
@@ -37,22 +37,38 @@ loader_server <- function(input, output, session, ...){
     # subject_code <- "DemoSubject"
     # report_filename <- "report-diagnostics_datetime-250811T165425_notch_filter"
     # 127.0.0.1:17283/?type=widget&project_name=test&subject_code=DemoSubject&report_filename=report-diagnostics_datetime-250811T165425_notch_filter&module=standalone_report&shared_id=kJKCof6lYRNqaFRst6Yr5Je6xp
+    # ?type=widget&project_name=YAEL&subject_code=yael_demo_001&report_name=electrodeview&module=standalone_report
 
     # validate the parameters
     subject <- ravecore::RAVESubject$new(project_name = project_name,
                                          subject_code = subject_code,
                                          strict = FALSE)
-    report_path <- file.path(subject$report_path, report_filename, "report.html")
-    local_data$report_path <- report_path
+    report_html <- NULL
+    if(length(report_filename)) {
+      report_html <- file.path(subject$report_path, report_filename, "report.html")
+    } else {
+      report_name <- query_list$query$report_name
+      reports <- list.files(
+        subject$report_path,
+        pattern = sprintf("^report-%s_datetime-[0-9]{6}T[0-9]{6}_", report_name)
+      )
+      if(length(reports)) {
+        report_filename <- sort(reports, decreasing = TRUE)[[1]]
+        report_html <- file.path(subject$report_path, report_filename, "report.html")
+      }
+    }
+
+    local_data$report_html <- report_html
 
     shiny::validate(
       shiny::need(dir.exists(subject$report_path), message = "Subject has no report path"),
-      shiny::need(grepl("^[a-zA-Z0-9_-]+$", report_filename), message = "Invalid report filename"),
-      shiny::need(length(report_path) == 1 && !is.na(report_path) &&
-                    file.exists(report_path), message = "Report path is invalid")
+      shiny::need(length(report_filename) == 1 && grepl("^[a-zA-Z0-9_-]+$", report_filename),
+                  message = "Invalid report filename"),
+      shiny::need(length(report_html) == 1 && !is.na(report_html) &&
+                    file.exists(report_html), message = "Report path is invalid")
     )
 
-    srcdoc <- paste(readLines(report_path), collapse = "\n")
+    srcdoc <- paste(readLines(report_html), collapse = "\n")
     shiny::tagList(
       shiny::div(
         style = "position:absolute;left:0;top:0;",
@@ -67,15 +83,15 @@ loader_server <- function(input, output, session, ...){
 
   output$download_btn <- shiny::downloadHandler(
     filename = function(...) {
-      if(!length(local_data$report_path)) { return("report.html") }
-      name <- basename(dirname(local_data$report_path))
+      if(!length(local_data$report_html)) { return("report.html") }
+      name <- basename(dirname(local_data$report_html))
       if(!endsWith(name, ".html")) {
         name <- sprintf("%s.html", name)
       }
       name
     },
     content = function(con) {
-      file.copy(local_data$report_path, con, overwrite = TRUE)
+      file.copy(local_data$report_html, con, overwrite = TRUE)
     }
   )
 
